2d8269a1fc1442687b4563c9cf5bba6cf206115b
Tori test <kynika.tasya@gmail.com>
2023-04-01 21:34:25 +0300

    change Test

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,8 +1,9 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import ru.netology.javaqa.BonusService;
 
 public class BonusServiceTest {
-    @org.junit.jupiter.api.Test
+    @Test
     public void shouldCalculateForRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
@@ -18,13 +19,46 @@ public class BonusServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void shouldCalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
         long amount = 1_000_000;
         boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateForUnregisteredAndUnderLimit() {
+
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 500;
+        boolean registered = false;
+        long expected = 5;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateForUnregisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 100_000;
+        boolean registered = false;
         long expected = 500;
 
         // вызываем целевой метод:
